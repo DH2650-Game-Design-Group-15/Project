0ac71a60c31612093e58ab9f42ff4221a3dc5a19
Linus Wallin <linuswallin@live.se>
2024-03-27 16:06:51 +0100

    feat: adds limit for scouting
    Adds a limit to how far from the base an NPC can walk when scouting
    for the player.

--- Assets/Scripts/AIScouting.cs
+++ Assets/Scripts/AIScouting.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,10 +8,14 @@ using UnityEngine.AI;
 public class AIScouting : MonoBehaviour {
     public NavMeshAgent human;
     public float speed;
+    public float maxDist;
     public float scoutRadius;
+    public Vector3 basePosition;
+    private Vector3 initPosition;
 
     private void Start () {
         human = GetComponent<NavMeshAgent>();
+        setInitPosition(human.transform.position);
         if (human != null) {
             human.speed = speed;
             human.SetDestination(RandomNavMeshLocation());
@@ -23,14 +28,38 @@ public class AIScouting : MonoBehaviour {
         }
     }
 
+    public void setInitPosition (Vector3 pos) {
+        initPosition = pos;
+    }
+
+    public Vector3 getInitPosition () {
+        return initPosition;
+    }
+
     public Vector3 RandomNavMeshLocation () {
-        Vector3 finalPosition = Vector3.zero;
-        Vector3 randomPosition = Random.insideUnitSphere * scoutRadius;
-        randomPosition += transform.position;
-        if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, scoutRadius, 1)) {
-            finalPosition = hit.position;
+        Vector3 finalPosition = getInitPosition();
+        Vector3 currentPosition = transform.position;
+        for (int i = 0; i < 30; i++){
+            Vector3 randomPosition = UnityEngine.Random.insideUnitSphere * maxDist;
+            randomPosition += currentPosition;
+            if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, maxDist, 1)) {
+                if (InsideScoutRadius(hit.position)) {
+                    finalPosition = hit.position;
+                    break;
+                } else {
+                    finalPosition = currentPosition;
+                }
+            }
         }
         return finalPosition;
+    }
+
+    public bool InsideScoutRadius (Vector3 pos) {
+        float dist = Vector3.Distance(pos, basePosition);
+        if (dist <= scoutRadius) {
+            return true;
+        }
+        return false;
     }
 
 }
