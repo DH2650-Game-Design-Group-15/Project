96eaa969b45991fa3695f641d6cf31f7a8b26613
Linus Wallin <linuswallin@live.se>
2024-05-20 16:57:06 +0200

    feat: adds death animation and damage take to npc

--- Assets/Resources/Scripts/NPC/BulletCollision.cs
+++ Assets/Resources/Scripts/NPC/BulletCollision.cs
@@ -10,7 +10,9 @@ public class BulletCollision : MonoBehaviour {
         if (collision.collider.CompareTag("Player")) {
             collision.collider.GetComponent<PlayerHealth>().PlayerHit(bulletDamage);
         }
-        Debug.Log("TEST");
+        if (collision.collider.CompareTag("NPC")) {
+            collision.collider.GetComponentInParent<AIScoutHealth>().TakeDamage(bulletDamage);
+        }
         Destroy(gameObject);  
     }
 
