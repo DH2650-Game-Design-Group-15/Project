98f12feba29c5c53a65f294abff1897e0fb3664c
Linus Wallin <73783914+LinusWallin@users.noreply.github.com>
2024-05-29 14:17:45 +0200

    105 ai bullet (#106)
    * fix: solves bug with AI bullets not hitting
    
    Removed bullets for AI and opted for a hitscan instead
    
    * fix: solves bug where quest UI isn't interactable
    
    * refactor: removes unnessesary debug prints and fixes some terrain
    
    * feat: updates quest rewards, availible items and misc

--- Assets/Resources/Scripts/Inventory/CraftableInventory.cs
+++ Assets/Resources/Scripts/Inventory/CraftableInventory.cs
@@ -60,8 +60,6 @@ public class CraftableInventory : MonoBehaviour
     }
     private void ButtonClicked(string buttonName)
     {
-
-        Debug.Log(buttonName + " was clicked!");
         bool LifePotionButton = false;
         switch (buttonName)
         {
